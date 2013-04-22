792a8d9d111758670f8fd50e5f50cf3537254524
Jacy <jacy.clare@gmail.com>
2013-04-22 12:08:12 -0400

    Initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,26 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def show
+    if params['student'] != nil
+      studentWage = params['student'].to_f
+    else
+      studentWage = 12.90
+    end
+    if params['actual'] != nil
+      actualWage = params['actual'].to_f
+    else
+      actualWage = 13.50
+    end
+    if params['hours'] != nil
+      hours = params['hours'].to_i
+    else
+      hours = 0
+    end
+    studentWage = 12.90 if studentWage == 0
+    actualWage = 13.50 if actualWage == 0
+    extraHours = ((hours*actualWage - hours*studentWage) / studentWage)
+    @roundedHours = (extraHours * 2.0).round / 2.0
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hours::Application.config.secret_token = '1a5d2f03e8c30d4a39e34d91c336d37e6a1b0df9a911b563d6592e4c41d1891c14df5dde45a76b83fe76ee23a4e5f28cb53aa941d90fe6e0130315d0c7a41570'
